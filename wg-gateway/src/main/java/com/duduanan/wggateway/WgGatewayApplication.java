fbbf76352422eaa59b7f72c376c4b151e428d30b
jerryguowei <jerryguowei@gmail.com>
2020-04-14 22:48:06 +1000

    add spring cloud gateway

---
+++ wg-gateway/src/main/java/com/duduanan/wggateway/WgGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.duduanan.wggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WgGatewayApplication.class, args);
+    }
+
+}
---
+++ wg-gateway/src/test/java/com/duduanan/wggateway/WgGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duduanan.wggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WgGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
