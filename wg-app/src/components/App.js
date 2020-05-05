4659c243761a6381111e9f0db372dc46708abea5
jerryguowei <jerryguowei@gmail.com>
2020-05-05 21:25:32 +1000

    init front-end app

---
+++ wg-app/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+
+Router
+
+const App = () => {
+
+    return (
+        <Router>
+            <div>
+                <Route path="/" exact component/>
+            </div>
+        </Router>
+    )
+};
+
+export default App;
